1a006dcc4e9ea11dfe7dafed20d5609b00224a0b
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2023-09-16 04:06:30 +0100

    Recipe & saved  modify

--- routes/recipes.js
+++ routes/recipes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const response = await RecipeModel.find({});
-    res.json(response.reverse());
+    res.json(response);
   } catch (err) {
     res.json(err);
   }
